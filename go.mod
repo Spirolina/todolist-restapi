65bc980587832465159d941542d27018990f4d06
Güner Arda Işık <g.isik.arda@gmail.com>
2023-06-28 19:12:03 +0300

    add models

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Spirolina/todolist-restapi
 
 go 1.20
+
+require go.mongodb.org/mongo-driver v1.12.0 // indirect
---
+++ models/todo.go
@@ -0,0 +1,9 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Todo struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Title    string             `bson:"title" json:"title"`
+	Complete bool               `bson:"complete" json:"complete"`
+}
