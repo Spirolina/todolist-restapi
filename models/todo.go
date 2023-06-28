9dddd45398b1e170606e6973360cc738c638a9a9
Güner Arda Işık <g.isik.arda@gmail.com>
2023-06-28 22:09:10 +0300

    fix add todo

--- models/todo.go
+++ models/todo.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Todo struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	ID       primitive.ObjectID `bson:"_id" json:"id"`
 	Title    string             `bson:"title" json:"title"`
 	Complete bool               `bson:"complete" json:"complete"`
 }
